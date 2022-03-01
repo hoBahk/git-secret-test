3e35ffbfbc381f17c24bc19967b42e10b684ba57
hoBahk <qudgh849@gmail.com>
2022-03-02 02:03:59 +0900

    initial Setting

---
+++ git-secret-test/git_secret_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_secret_testApp.swift
+//  git-secret-test
+//
+//  Created by 박병호 on 2022/03/02.
+//
+
+import SwiftUI
+
+@main
+struct git_secret_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
